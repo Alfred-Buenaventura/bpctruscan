93e429021869b343074c2d4ba13341b60cf3a025
Alfred-Buenaventura <alfredprime2003@gmail.com>
2026-02-11 19:11:09 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ob_start();
 require_once __DIR__ . '/app/init.php';
 require_once 'app/controllers/dashboardcontroller.php';
 $dashboard = new DashboardController();
